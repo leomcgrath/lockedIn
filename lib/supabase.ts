d0c157643c1959e070777a771a488f1f41877bbb
“leomcgrath” <leo.mcgrath@yahoo.no>
2025-06-19 12:43:39 +0200

    Changed variablenames for supabasekey

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://tpjyejgrnwssnbnuxsfq.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY ?? '';
+const EXPO_PUBLIC_SUPABASE_KEY = process.env.SUPABASE_KEY ?? '';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, EXPO_PUBLIC_SUPABASE_KEY);
